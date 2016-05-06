807fae9d4bad97330bcf18d50a117e45843b5a7f
c ma <mac@yydemacbook-pro-4.local>
2016-05-06 22:23:49 +0800

    初始化

---
+++ lianxi/lianxi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lianxi
+//
+//  Created by ma c on 16/5/6.
+//  Copyright © 2016年 YYN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lianxi/lianxi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lianxi
+//
+//  Created by ma c on 16/5/6.
+//  Copyright © 2016年 YYN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
